f97f18a39dac26e21e489f633dd43fb08a1b16b3
chinu27 <infy.chinmaysharma@gmail.com>
2019-02-02 02:14:33 +0530

    version 0.0.1

--- warhunter.py
+++ warhunter.py
@@ -1,9 +1,9 @@
 import praw
 
 reddit = praw.Reddit('whbot')
-subreddit = r.subreddit("csgo")
+subreddit = reddit.subreddit("csgo")
 
-for submission in subreddit.hot(limit=5):
+for submission in subreddit.hot(limit=7):
     print("Title: ", submission.title)
     print("Text: ", submission.selftext)
     print("Score: ", submission.score)
